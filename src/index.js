f4165ba66a091c15027a1ba8b75352908b0f5838
Carlos.Inhani <carlos.inhani@gmail.com>
2020-11-03 19:57:14 -0300

    number_8

--- src/index.js
+++ src/index.js
@@ -1,8 +1,17 @@
 import 'react-native-gesture-handler';
-import React from 'react';
+import React, { Component } from 'react';
+import { StatusBar } from 'react-native';
 
 import Routes from './routes';
 
-const App = () => <Routes />;
+const App = () => {
+  return (
+    <>
+      <StatusBar barStyle="light-content" backgroundColor="#312e38" />
+      <Routes />
+    </>
+  );
+};
+
 
 export default App;
